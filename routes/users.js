62f15e24f50c64b5d4a420e47c6b9f28c84415d5
medha1403 <83916782+medha1403@users.noreply.github.com>
2022-01-17 14:37:43 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+let mongoose=require('mongoose');
+let url='mongodb+srv://pratiknew:pratik@pratik.7m6lk.mongodb.net/gamingforum9to12?retryWrites=true&w=majority'
+mongoose.connect(url,{useUnifiedTopology:true,useNewUrlParser:true})
+.then(function(){
+  console.log('Database Connected')
+})
+.catch(function(e){
+  console.log(e)
+})
+let UserSchema=mongoose.Schema({
+  gamename:String,
+  review:String
+})
+module.exports=mongoose.model('user',UserSchema)
